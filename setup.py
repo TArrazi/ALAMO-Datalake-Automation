99eb329175eefc7de3a120918682d5e0d6c88e55
TiagoCosta <tiago.costa@semantix.ai>
2023-01-12 11:17:29 -0300

    Added wheel files for testing purposes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='alamo',
+    version='0.0.1',
+    description='Simple tools for automating jarring e repetitive datalake tasks',
+    url='https://github.com/TArrazi/ALAMO-Datalake-Automation',
+    author='Tiago Costa Arrazi',
+    author_email='N/A',
+    packages=['src'],
+)
